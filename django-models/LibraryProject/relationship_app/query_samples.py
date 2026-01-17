ea0b4f720cea388f729ffd908d9947888b2bd15a
Benjamin Appiah <ben.appiah451@gmail.com>
2026-01-17 15:46:41 +0000

    Fixed Relationshi_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -15,5 +15,5 @@ def query_books_in_library(library_name):
 
 def query_librarian_for_library(library_name):
     """Retrieve the librarian assigned to a specific library."""
-    librarian = Librarian.objects.get(library__name=library_name)
+    librarian = Librarian.objects.get(library=library_name)
     return librarian
