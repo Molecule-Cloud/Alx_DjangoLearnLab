7c0d54130472f80810a0851baa71ae879277b4bd
Benjamin Appiah <ben.appiah451@gmail.com>
2026-01-18 18:39:41 +0000

    FIXED Import statement

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views
+from .views import views
+from.views import LibraryDetailView
 
 urlpatterns = [
     # Function-based view: Shows ALL books
     path('books/', views.list_books, name='book-list'),
     
     # Class-based view: Shows ONE specific library (needs ID)
-    path('library/<int:pk>/', views.BookDetailView.as_view(), name='library-detail'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
     # ↑ Added: <int:pk> to tell Django WHICH library
 ]
