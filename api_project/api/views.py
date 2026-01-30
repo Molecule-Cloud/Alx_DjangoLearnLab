2656908021abf0a85bc034d23a526c0156baadb3
Benjamin Appiah <ben.appiah451@gmail.com>
2026-01-30 17:23:45 +0000

    Fixed function import statement in api/views.py file

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from api.models import Book
-from api.serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+from .serializers import BookSerializer
+from rest_framework import generics
 
 # == BookList View that returns data using the seializer == #
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
