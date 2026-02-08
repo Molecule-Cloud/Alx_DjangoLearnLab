2ec458d284dce139d7d81f5567c43ac1a945874a
Benjamin Appiah <ben.appiah451@gmail.com>
2026-02-08 03:24:14 +0000

    Fixed Custom Views with Generics and Mixins

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    # RESTful book endpoints
-    path('books/', views.BookListView.as_view(), name='book-list'),  # GET list, POST create
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # GET, PUT, PATCH, DELETE
+    # Book URLs
+    path('books/', views.BookListView.as_view(), name='book-list'),  # GET all books
+    path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # POST new book
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # GET one book
+    path('books/<int:pk>/update/', views.BookDetailView.as_view(), name='book-update'),  # PUT/PATCH update
+    path('books/<int:pk>/delete/', views.BookDetailView.as_view(), name='book-delete'),  # DELETE
     
-    # Author endpoints
+    # Author URLs
     path('authors/', views.AuthorListView.as_view(), name='author-list'),
 ]
