8821ab7865bc3d0568f39ae97002f8bdd36d866a
Benjamin Appiah <ben.appiah451@gmail.com>
2026-02-08 03:25:05 +0000

    Fixed Custom Views with Generics and Mixins

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='book-list'),  # GET all books
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # POST new book
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # GET one book
-    path('books/<int:pk>/update/', views.BookDetailView.as_view(), name='book-update'),  # PUT/PATCH update
-    path('books/<int:pk>/delete/', views.BookDetailView.as_view(), name='book-delete'),  # DELETE
+    path('books/<int:pk>/update/', views.BookDetailView.as_view(), name='book/update'),  # PUT/PATCH update
+    path('books/<int:pk>/delete/', views.BookDetailView.as_view(), name='book/delete'),  # DELETE
     
     # Author URLs
     path('authors/', views.AuthorListView.as_view(), name='author-list'),
