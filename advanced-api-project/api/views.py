9e024df49e084f200c4a95b7a9c51ea4455f76a9
Benjamin Appiah <ben.appiah451@gmail.com>
2026-02-08 03:29:39 +0000

    Fixed Custom Views with Generics and Mixins

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import IsAuthenticatedOrReadOnly ,IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # == Generic API Views ==
 class ListView(generics.ListAPIView):
