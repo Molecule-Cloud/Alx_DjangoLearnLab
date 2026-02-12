7c0ebe35aaf2cd47b24a1706f9dd44719027a6c7
Benjamin Appiah <ben.appiah451@gmail.com>
2026-02-12 16:15:22 +0000

    Scaffolded djanog_blog project

---
+++ django_blog/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+# == User Model == #
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+
+# == Post Model == #
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    content = models.TextField(max_length=100)
+    published_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
